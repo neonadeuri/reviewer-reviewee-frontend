4cee9b2d3e7a9a504a8b5b7f62e97fa45b33386c
kuku <covernat6949@gmail.com>
2023-03-01 01:23:28 +0900

    feat : Reviewer Register 페이지 생성

---
+++ pages/register.tsx
@@ -0,0 +1,11 @@
+import RegisterTemplate from '../components/reviewerRegister/RegisterTemplate';
+
+const ReviewerRegister = () => {
+  return (
+    <div className="h-full flex justify-center mt-14 px-6">
+      <RegisterTemplate />
+    </div>
+  );
+};
+
+export default ReviewerRegister;
