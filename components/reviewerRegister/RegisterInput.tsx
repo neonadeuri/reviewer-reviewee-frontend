686dda9c8724782e63b53333d96ec351a1ca9819
kuku <covernat6949@gmail.com>
2023-03-01 01:28:35 +0900

    feat : register Form component 생성

---
+++ components/reviewerRegister/RegisterInput.tsx
@@ -0,0 +1,21 @@
+import { IRegisterInputType } from './reviewerRegister';
+
+function RegisterInput({ name, input, setInput, placeholder }: IRegisterInputType) {
+  return (
+    <div className="mt-8">
+      <label htmlFor="user-id" className="w-full flex flex-col items-start">
+        {name}
+      </label>
+      <input
+        id="user-id"
+        type="text"
+        className="p-2 h-10 w-full border-solid border-2 rounded-md"
+        placeholder={placeholder}
+        onChange={(e) => setInput(e.currentTarget.value)}
+        value={input}
+      />
+    </div>
+  );
+}
+
+export default RegisterInput;
