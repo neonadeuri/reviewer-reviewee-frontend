98b8f5754819202f60fd6f8792eff094e8222102
kuku <covernat6949@gmail.com>
2023-03-01 01:27:51 +0900

    feat : RegisterTemplate 생성

---
+++ components/reviewerRegister/RegisterTemplate.tsx
@@ -0,0 +1,12 @@
+import RegisterForm from './RegisterForm';
+
+const RegisterTemplate = () => {
+  return (
+    <div className="h-full w-full max-w-md">
+      <h2 className="text-center text-3xl">리뷰어 등록</h2>
+      <RegisterForm />
+    </div>
+  );
+};
+
+export default RegisterTemplate;
