5c9b38c71b05e2c23797c4cc8fdf4729d1932526
leeuihyun <meta212121@naver.com>
2023-02-28 17:25:30 +0900

    feat : Header component 생성

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,25 @@
+import Image from 'next/image';
+import img from 'styles/images/person.png';
+
 const Header = () => {
   return (
-    <div className="w-full h-16 sticky border-b border-gray-200 px-6">
-      <div className="max-w-7xl mx-auto h-full flex items-center justify-center">Header</div>
+    <div className="w-full sticky mb-3">
+      <div className="max-w-7xl mx-auto h-24 flex items-center justify-between text-4xl">
+        <span className="cursor-pointer font-bold">Logo</span>
+        <span className="cursor-pointer font-bold">LogIn</span>
+      </div>
+      <div className="w-full h-80 bg-cyan-200">
+        <div className="max-w-7xl h-full flex items-center justify-between mx-auto">
+          <Image src={img} alt="person" />
+          <div className="w-full text-4xl font-bold leading-10 tracking-widest">
+            <div className="w-full flex justify-center items-center flex-col">
+              <span>개인 프로젝트의 코드리뷰가</span>
+              <br />
+              <span>필요하신가요?</span>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
