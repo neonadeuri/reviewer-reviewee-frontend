7a2fa43cb5bf0d97e2d8d1d83fbae11064724f49
kuku <covernat6949@gmail.com>
2023-03-02 17:08:00 +0900

    refactor : 파일명 수정 및 유저 정보화면으로 리팩토링

---
+++ components/userInfomation/InformationForm.tsx
@@ -0,0 +1,59 @@
+import { useState } from 'react';
+import RegisterInput from './UserInput';
+import ReviewerRegisterModal from './ReviewerRegisterModal';
+
+const InformationForm = () => {
+  // User Information Form
+  const [userId, setUserId] = useState<string>('');
+  const [email, setEmail] = useState<string>('');
+  const [url, setUrl] = useState<string>('');
+  const [modal, setModal] = useState<boolean>(false);
+  const [modify, setModify] = useState<boolean>(false);
+  return (
+    <div>
+      <div>
+        {modify ? (
+          <>
+            <RegisterInput
+              name="활동명"
+              input={userId}
+              setInput={setUserId}
+              placeholder="활동명을 입력해주세요."
+              disable={false}
+            />
+            <RegisterInput
+              name="이메일"
+              input={email}
+              setInput={setEmail}
+              placeholder="이메일을 입력해주세요."
+              disable={false}
+            />
+            <RegisterInput name="Github URL" input={url} setInput={setUrl} />
+          </>
+        ) : (
+          <>
+            <RegisterInput name="활동명" input={userId} setInput={setUserId} />
+            <RegisterInput name="이메일" input={email} setInput={setEmail} />
+            <RegisterInput name="Github URL" input={url} setInput={setUrl} />
+          </>
+        )}
+      </div>
+      <div className="mt-8 flex gap-2">
+        <button
+          onClick={() => setModify((prev) => !prev)}
+          className="w-full flex justify-center items-center bg-black text-white h-10 rounded-md">
+          {modify ? '수정 반영' : '정보 수정'}
+        </button>
+        <button
+          onClick={() => setModal(true)}
+          type="submit"
+          className="w-full flex justify-center items-center bg-black text-white h-10 rounded-md">
+          리뷰어 등록
+        </button>
+      </div>
+      {modal && <ReviewerRegisterModal setModal={setModal} />}
+    </div>
+  );
+};
+
+export default InformationForm;
