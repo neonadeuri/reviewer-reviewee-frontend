caa1c200cd196a2aaab1e66fbb74c2f6e1fa531a
kuku <covernat6949@gmail.com>
2023-03-02 16:57:15 +0900

    refactor : register => user 로 변경

---
+++ pages/user.tsx
@@ -0,0 +1,11 @@
+import InformationTemplate from '../components/userInfomation/InformationTemplate';
+
+const UserPage = () => {
+  return (
+    <div className="h-full flex justify-center mt-14 px-6">
+      <InformationTemplate />
+    </div>
+  );
+};
+
+export default UserPage;
