505117503d20e6a34ceac8234acea13c44741ef6
leeuihyun <meta212121@naver.com>
2023-03-02 16:47:12 +0900

    feat : 깃허브 로그인 관리 페이지(로딩 or 리다이렉트)

--- pages/githubLogin.tsx
+++ pages/githubLogin.tsx
@@ -1,15 +1,8 @@
-import { useRouter } from 'next/router';
-import React, { useEffect } from 'react';
+import React from 'react';
+import Template from '../components/LogInRedirect/Template';
 
 function GithubLogin() {
-  const router = useRouter();
-
-  useEffect(() => {
-    console.log(router.query.code);
-  }, [router]);
-
-  //전역 component로 로딩을 만들어서 import 사용할 생각입니다.
-  return <div>로딩중</div>;
+  return <Template />;
 }
 
 export default GithubLogin;
