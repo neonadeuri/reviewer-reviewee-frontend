88942ca7a1987af5f77f5657cc4033c182d298e9
leeuihyun <meta212121@naver.com>
2023-03-02 17:12:25 +0900

    refactor : useHooks - useSetLocalPath로 기존 코드 리팩토링

--- components/Main/Template.tsx
+++ components/Main/Template.tsx
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
 import FilterCategory from './FilterCategory';
 import CategoryBox from './CategoryBox';
+import useSetLocalPath from '../../useHooks/useSetLocalPath';
 
 function Template() {
   const [category, setCategory] = useState<string>('');
-
+  useSetLocalPath();
   return (
     <div className="w-full h-full max-w-7xl">
       <FilterCategory setCategory={setCategory} />
