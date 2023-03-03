1785e97bd8138e23d3d9732ca54d41f766fc5610
leeuihyun <meta212121@naver.com>
2023-03-03 18:39:53 +0900

    feat : 간단한 목업 데이터 로그인 요청 및 로직 작성

--- useHooks/useRedirectBefore.tsx
+++ useHooks/useRedirectBefore.tsx
@@ -1,13 +1,20 @@
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
+import logIn from '../pages/api/logIn';
 
 function useRedirectBefore() {
   const router = useRouter();
 
   return useEffect(() => {
     if (router.query.code) {
-      //router.replace(localStorage.getItem('path') || '/');
       console.log(router.query.code);
+      logIn(router.query.code).then((res) => {
+        console.log(res?.status);
+        if (res?.status === 200) {
+          //router.replace(localStorage.getItem('path') || '/');
+          //logIn 하기 이전 페이지로 이동
+        }
+      });
     }
   }, [router.query.code]);
 }
