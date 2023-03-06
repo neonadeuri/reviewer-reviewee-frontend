5d5a751b80c619dd8b5e4ad3153a93e5092c7583
leeuihyun <meta212121@naver.com>
2023-03-06 20:44:49 +0900

    feat : 목업 API 추가

--- mocks/handlers.ts
+++ mocks/handlers.ts
@@ -12,4 +12,26 @@ export const handlers = [
       }),
     );
   }),
+
+  rest.get('http://localhost:3000/members', (req, res, ctx) => {
+    console.log('members');
+    return res(
+      ctx.status(200),
+      ctx.json({
+        url: 'image.png',
+      }),
+    );
+  }),
+
+  rest.post('http://localhost:3000/auth/refresh', (req, res, ctx) => {
+    console.log('refresh');
+    return res(
+      ctx.status(200),
+      ctx.cookie('access_token', 'ACCESS_TOKEN'),
+      ctx.cookie('refresh_token', 'REFRESH_TOKEN'),
+      ctx.json({
+        message: '로그인이 만료되어 access Token을 재발급받았습니다.',
+      }),
+    );
+  }),
 ];
