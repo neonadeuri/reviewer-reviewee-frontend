d141de1b08d9a62e9a8709732f9a75ddb52552e3
leeuihyun <meta212121@naver.com>
2023-03-07 19:00:07 +0900

    refactor : Header 로그인 유지를 위한 props 타입 리팩토링

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,17 +1,26 @@
 import Image from 'next/image';
 import img from 'styles/images/person.png';
 import Link from 'next/link';
+import { IAllPagePropsData } from '../globalType';
 
-const Header = () => {
+const Header = ({ image_url, isLogIn }: IAllPagePropsData) => {
+  console.log(isLogIn);
   return (
     <div className="w-full sticky mb-3">
       <div className="max-w-7xl mx-auto h-24 flex items-center justify-between text-4xl">
         <Link href="/">
           <span className="cursor-pointer font-bold">Logo</span>
         </Link>
-        <Link href={'/login'}>
-          <span className="cursor-pointer font-bold">LogIn</span>
-        </Link>
+        {isLogIn === true ? (
+          <Link href={'/login'}>
+            {/* 정상적으로 로그인이 되어있다면 image_url 이미지를 띄워줄 예정 (next/Image) 로그인시 클릭하여 드롭다운 구현할 예정*/}
+            <span className="cursor-pointer font-bold">{image_url}</span>
+          </Link>
+        ) : (
+          <Link href={'/login'}>
+            <span className="cursor-pointer font-bold">LogIn</span>
+          </Link>
+        )}
       </div>
       <div className="w-full h-80 bg-cyan-200">
         <div className="max-w-7xl h-full flex items-center justify-between mx-auto">
