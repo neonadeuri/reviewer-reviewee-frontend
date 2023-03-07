037c0287083be43f2841d949738036638a080bbb
leeuihyun <meta212121@naver.com>
2023-03-07 19:00:55 +0900

    feat : 로그인 유지,토큰 만료에 API 생성

--- mocks/handlers.ts
+++ mocks/handlers.ts
@@ -14,17 +14,27 @@ export const handlers = [
   }),
 
   rest.get('http://localhost:3000/members', (req, res, ctx) => {
-    console.log('members');
+    console.log('access token 만료 API');
+    return res(
+      ctx.status(401),
+      ctx.json({
+        image_url: 'image.png',
+      }),
+    );
+  }),
+
+  rest.get('http://localhost:3000/members1', (req, res, ctx) => {
+    console.log('정상 로그인');
     return res(
       ctx.status(200),
       ctx.json({
-        url: 'image.png',
+        image_url: 'image.png',
       }),
     );
   }),
 
   rest.post('http://localhost:3000/auth/refresh', (req, res, ctx) => {
-    console.log('refresh');
+    console.log('access token 만료시 refresh API');
     return res(
       ctx.status(200),
       ctx.cookie('access_token', 'ACCESS_TOKEN'),
