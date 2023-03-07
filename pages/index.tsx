e5928c9428fef7603a1738f917b0dea439630e83
leeuihyun <meta212121@naver.com>
2023-03-07 18:59:37 +0900

    feat : 로그인 유지 세부로직

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,46 @@
-import type { NextPage } from 'next';
 import Template from '../components/Main/Template';
+import Header from '../components/Header';
+import { GetServerSideProps } from 'next';
+import axios from 'axios';
+import { IData, IAllPagePropsData } from '../globalType';
+import logInUser from './api/logInUser';
 
-const Home: NextPage = () => {
+const Home = ({ image_url, isLogIn }: IAllPagePropsData) => {
+  console.log(image_url, isLogIn);
   return (
-    <div className="w-full h-full flex justify-center">
-      <Template />
-    </div>
+    <>
+      <Header image_url={image_url} isLogIn={isLogIn}></Header>
+      <div className="w-full h-full flex justify-center">
+        <Template />
+      </div>
+    </>
   );
+};
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const cookie = context.req ? context.req.headers.cookie : '';
+  axios.defaults.headers.Cookie = '';
+  if (context.req && cookie) {
+    axios.defaults.headers.Cookie = cookie;
+    const response = await logInUser();
+    const data: IData = response?.data;
+    const image_url = data.image_url;
+    const isLogIn = true;
+    return {
+      props: {
+        image_url,
+        isLogIn,
+      },
+    };
+  }
+  const image_url = '';
+  const isLogIn = false;
+  return {
+    props: {
+      image_url,
+      isLogIn,
+    },
+  };
 };
 
 export default Home;
