2d275b78390c4101cf2b6b402a8c410bb9aeb623
kuku <covernat6949@gmail.com>
2023-03-09 00:22:22 +0900

    refactor : 리뷰어 등록 react query 사용으로 수정

--- components/userInfomation/ReviewerRegisterModal.tsx
+++ components/userInfomation/ReviewerRegisterModal.tsx
@@ -1,32 +1,43 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import { AxiosResponse } from 'axios';
 import { useEffect, useRef, useState } from 'react';
-import { registerUpdate } from '../../pages/api/userInfo';
-import { IModalPropsType, IReviewerRegisterUpdateType, ISkillType } from './informationType';
+import { registerUpdate, reviewerGet, reviewerRegister } from '../../pages/api/userInfo';
+import {
+  IModalPropsType,
+  IRegister,
+  IRegisterMutationProps,
+  IReviewerRegisterUpdateType,
+  ISkillType,
+} from './informationType';
 import ReviewerDropDown from './ReviewerDropDown';
 
-function ReviewerRegisterModal({ setModal, data }: IModalPropsType) {
-  const modalRef = useRef<HTMLDivElement>(null);
-  const [job, setJob] = useState<string>(data.register.job);
-  const [etc, setEtc] = useState<string>('');
-  const [career, setCareer] = useState<string>(data.register.career);
-  const [techStack, setTechStack] = useState<ISkillType[]>(data.register.techStack);
-  const [introduction, setIntroduction] = useState<string>(data.register.introduction);
+function ReviewerRegisterModal({ setModal }: IModalPropsType) {
+  const queryClient = useQueryClient();
+  const { data, isLoading } = useQuery<IRegister>({
+    queryKey: ['reviewer'],
+    queryFn: () => reviewerGet(),
+    staleTime: 5000,
+  });
 
-  const { mutate, isLoading } = useMutation({
-    mutationFn: (register: IReviewerRegisterUpdateType) => {
-      return registerUpdate(register);
+  const { mutate } = useMutation({
+    mutationFn: ({ register, mutationFnCb }: IRegisterMutationProps) => {
+      return mutationFnCb(register);
     },
     onSuccess: () => {
       setModal(false);
+      queryClient.invalidateQueries(['reviewer']);
     },
     onError: () => {
-      setJob(data.register.job);
-      setCareer(data.register.career);
-      setTechStack(data.register.techStack);
-      setIntroduction(data.register.introduction);
       alert('오류로 인해 업데이트가 진행되지 않았습니다.');
     },
   });
+
+  const modalRef = useRef<HTMLDivElement>(null);
+  const [job, setJob] = useState<string>('');
+  const [etc, setEtc] = useState<string>('');
+  const [career, setCareer] = useState<string>('');
+  const [techStack, setTechStack] = useState<ISkillType[]>([]);
+  const [introduce, setIntroduce] = useState<string>('');
 
   useEffect(() => {
     const handler = (event: any) => {
@@ -41,18 +52,30 @@ function ReviewerRegisterModal({ setModal, data }: IModalPropsType) {
     };
   }, []);
 
+  useEffect(() => {
+    if (data) {
+      setJob(data.job);
+      setCareer(data.career);
+      setIntroduce(data.introduce);
+      setTechStack(data.techStack);
+    }
+  }, [data]);
+
   // reviewer 등록 함수
-  const reviewerSubmit = () => {
+  const reviewerSubmit = (mutationFn: (regi: IReviewerRegisterUpdateType) => Promise<AxiosResponse<any, any>>) => {
     mutate({
-      job: etc ? etc : job,
-      career: career,
-      techStack: techStack.map((el) => el.id),
-      introduction: introduction,
+      register: {
+        job: etc ? etc : job,
+        career: career,
+        techStack: techStack.map((el) => el.id),
+        introduce: introduce,
+      },
+      mutationFnCb: mutationFn,
     });
   };
 
   const checkValidation = (): boolean => {
-    if (job && career && techStack && introduction) {
+    if (job && career && techStack && introduce) {
       return true;
     } else {
       alert('전부 필수 내용입니다.');
@@ -70,50 +93,54 @@ function ReviewerRegisterModal({ setModal, data }: IModalPropsType) {
       <div
         ref={modalRef}
         className="absolute w-[32rem] h-[38rem] border-solid border-2 rounded-md z-50 left-2/4 top-2/4 -translate-y-2/4 -translate-x-2/4 bg-white msm:w-11/12">
-        <div className="w-full h-full p-7 flex flex-col">
-          <div className="text-center text-2xl">리뷰어 등록</div>
-          <div className="overflow-y-auto flex flex-col h-full">
-            <ReviewerDropDown
-              select={job}
-              setSelect={(value: React.SetStateAction<string | ISkillType[]>) => setJob(value as string)}
-              name="직무"
-              dropList={data.registerOption.positionList}
-              etc={etc}
-              setEtc={setEtc}
-              ment="직무를 선택해주세요"
-            />
-            <ReviewerDropDown
-              select={career}
-              setSelect={(value: React.SetStateAction<string | ISkillType[]>) => setCareer(value as string)}
-              name="경력"
-              dropList={data.registerOption.careerList}
-              ment="경력을 선택해주세요"
-            />
-            <ReviewerDropDown
-              select={techStack}
-              setSelect={(value: React.SetStateAction<string | ISkillType[]>) => setTechStack(value as ISkillType[])}
-              name="기술 스택"
-              dropList={data.registerOption.techList}
-              ment="스킬을 선택해주세요"
-            />
-            <div className="mt-6">
-              <span className="w-full flex flex-col items-start">소개글</span>
-              <textarea
-                className="p-2 w-full h-20 border-solid border-2 rounded-md outline-none"
-                value={introduction}
-                onChange={(e) => setIntroduction(e.currentTarget.value)}
+        {data && (
+          <div className="w-full h-full p-7 flex flex-col">
+            <div className="text-center text-2xl">리뷰어 등록</div>
+            <div className="overflow-y-auto flex flex-col h-full">
+              <ReviewerDropDown
+                select={job}
+                setSelect={(value: React.SetStateAction<string | ISkillType[]>) => setJob(value as string)}
+                name="직무"
+                dropList={data.positionList}
+                etc={etc}
+                setEtc={setEtc}
+                ment="직무를 선택해주세요"
               />
+              <ReviewerDropDown
+                select={career}
+                setSelect={(value: React.SetStateAction<string | ISkillType[]>) => setCareer(value as string)}
+                name="경력"
+                dropList={data.careerList}
+                ment="경력을 선택해주세요"
+              />
+              <ReviewerDropDown
+                select={techStack}
+                setSelect={(value: React.SetStateAction<string | ISkillType[]>) => setTechStack(value as ISkillType[])}
+                name="기술 스택"
+                dropList={data.techList}
+                ment="스킬을 선택해주세요"
+              />
+              <div className="mt-6">
+                <span className="w-full flex flex-col items-start">소개글</span>
+                <textarea
+                  className="p-2 w-full h-20 border-solid border-2 rounded-md outline-none"
+                  value={introduce}
+                  onChange={(e) => setIntroduce(e.currentTarget.value)}
+                />
+              </div>
+            </div>
+            <div className="text-center flex justify-center">
+              <button
+                className="w-full flex justify-center items-center bg-black text-white h-10 rounded-md"
+                onClick={() =>
+                  checkValidation() && data.job ? reviewerSubmit(registerUpdate) : reviewerSubmit(reviewerRegister)
+                }>
+                {/* 추후 정보가 있는지 없는지 여부에 따른 멘트 변경 */}
+                리뷰어 등록
+              </button>
             </div>
           </div>
-          <div className="text-center flex justify-center">
-            <button
-              className="w-full flex justify-center items-center bg-black text-white h-10 rounded-md"
-              onClick={() => checkValidation() && reviewerSubmit()}>
-              {/* 추후 정보가 있는지 없는지 여부에 따른 멘트 변경 */}
-              리뷰어 등록
-            </button>
-          </div>
-        </div>
+        )}
       </div>
     </div>
   );
