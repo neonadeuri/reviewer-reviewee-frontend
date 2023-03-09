748394edbfac3999380ee5b71873397181bc0b02
kuku <covernat6949@gmail.com>
2023-03-09 20:14:35 +0900

    refactor : 동적 라우팅에서 정적 라우팅으로 변경

--- pages/user/me.tsx
+++ pages/user/me.tsx
@@ -12,18 +12,24 @@ const UserPage = ({ data }: UserPageProps) => {
 };
 
 export const getServerSideProps: GetServerSideProps = async () => {
-  const user = await userGet();
-  // 처음에 데이터를 전부 가져오려고 했으나 리뷰어 쪽을 누르지않는다면 불필요한 API 요청이 발생한다고 생각하여 주석처리
-  // const reviewer = reviewerGet();
-  // const res = await Promise.all([user, reviewer]);
-  const data: IUserGetType = {
-    userInfo: user,
-  };
-  return {
-    props: {
-      data,
-    },
-  };
+  try {
+    const user = await userGet();
+    const data: IUserGetType = {
+      userInfo: user,
+    };
+    return {
+      props: {
+        data,
+      },
+    };
+  } catch (err) {
+    console.error(err);
+    return {
+      props: {
+        userInfo: '',
+      },
+    };
+  }
 };
 
 export default UserPage;
