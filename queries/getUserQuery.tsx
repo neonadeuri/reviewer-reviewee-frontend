4558a684f2aeca8d1c43945355fa05cafc4754e4
leeuihyun <meta212121@naver.com>
2023-03-10 19:05:23 +0900

    refactor : refresh Token 만료 로직까지 수정

--- queries/getUserQuery.tsx
+++ queries/getUserQuery.tsx
@@ -1,12 +1,15 @@
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
 import { logInUser, refreshUser } from '../pages/api/member';
 import { getAccessToken, setAccessToken } from '../utils/authLogic';
 import { AxiosError } from 'axios';
+import { useRouter } from 'next/router';
 
 const USER_KEY = 'user';
 
 function useGetUserQuery() {
   const access_token = getAccessToken();
+  const queryClient = useQueryClient();
+  const router = useRouter();
 
   return useQuery([USER_KEY, 'USE_ACCESS_TOKEN'], logInUser, {
     enabled: !!access_token,
@@ -14,9 +17,14 @@ function useGetUserQuery() {
     onError: async (error) => {
       const { response } = error as unknown as AxiosError;
       if (response?.status === 401) {
-        const res: string = await refreshUser();
-        setAccessToken(res);
-        return useGetUserQuery;
+        try {
+          const res: string = await refreshUser();
+          setAccessToken(res);
+          queryClient.invalidateQueries(['USER_KEY', 'USE_ACCESS_TOKEN']);
+        } catch (error) {
+          alert('리프레시 토큰 만료 / 새로 로그인');
+          router.push('/');
+        }
       }
     },
   });
